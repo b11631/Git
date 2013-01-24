4fdaf9ec9618310d72cceebccdeb2a34d0b672b3
Donald Moore <donaldmoore@Donalds-MacBook-Pro.local>
2013-01-24 13:48:57 -0600

    Added files to repo

---
+++ Project 3/donald_moore_assignment3.js
@@ -0,0 +1,40 @@
+// Donald Moore
+// January 24, 2013
+// Project 3
+// Out for Drinks
+
+var friend1 = {
+      name: "Mike",
+      age: 22,
+      drinkPref: "Beer",
+      ate: false
+};
+var friend2 = {
+    name: "John",
+    age: 23,
+    drinkPref: "Hard liquor",
+    ate: false
+};
+var me = {
+    name: "Me",
+    age: 24,
+    drinkPref: "Beer",
+    ate: false
+};
+var designatedDriver = {
+    name: "Seth",
+    age: 20,
+    drinkPref: "Coke",
+    ate: false
+};
+var bar1 = {
+    name: "Sports Bar",
+    food: true,
+    beer: true,
+    hardLiqour: false,
+    
+};
+
+console.log( me.name, ' and my friends ' + friend1.name +' and ' + friend2.name + ' decided to go out for drinks.');
+console.log("The first place we visited was the" + bar1.name);
+
