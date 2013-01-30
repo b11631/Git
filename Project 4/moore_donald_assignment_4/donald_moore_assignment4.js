17cae75f11fefc0c52b32ab439eedf5cef92d8d6
Donald Moore <donaldmoore@Donalds-MacBook-Pro.local>
2013-01-30 10:25:00 -0600

    Project 4 finished string library

--- Project 4/moore_donald_assignment_4/donald_moore_assignment4.js
+++ Project 4/moore_donald_assignment_4/donald_moore_assignment4.js
@@ -15,14 +15,37 @@ var stringLibrary = function () {
 	    }
     };
     
-    //Does a string follow an aaa@bbb.ccc pattern like an email address?
-    //Returns Boolean
-    var validateEmail = function (string) {
+	//Does a string follow an aaa@bbb.ccc pattern like an email address?
+	//Returns Boolean
+	var validateEmail = function (string) {
 	    var isWhiteSpace = /\s+/g;  // Tests for any white space in the string.
 	    if (((string.indexOf("@") !== -1) && (isWhiteSpace.test(string) === false) && ((string.lastIndexOf(".")) === (string.length - 4)))) {
 		    return true;
 	    } else {
 		    return false;
 	    }
+    };
+	
+	 //  VALIDATE URL (Does it start with http: or https:?)
+	var validateURL = function (string) {
+	    var isHttp = /^http:\/\//,  // Match http:\\ and https:\\ starting at the beginning of the string.
+		    isHttps = /^https:\/\//,
+		    isWhiteSpace = /\s+/g;  // Tests for any white space in the string.
+	    if ((((isHttp.test(string)) || (isHttps.test(string)))) && (isWhiteSpace.test(string) === false)) {
+		    return true;
+	    } else {
+		    return false;
+	    }
+    };
+	
+	//  TITLE CASE STRING (Split into words, then uppercase the first letter of each word.)
+    var titleCaseString = function (string) {  // STILL NEED TO MAKE THIS WORK PROPERLY
+	    var splitString = string.split(" "),
+		    newString,
+		    i;
+	    for (i = 0; i < splitString.length; i += 1) {
+		    newString = splitString[i][0].toUpperCase();
+	    }
+	    return newString;
     };
 };
